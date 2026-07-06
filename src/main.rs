e7653f2c9088d1c310b4e742b41b72db24ffc34c
krisjanisp <krisjanispetrucena@gmail.com>
2026-07-06 21:04:19 +0300

    add import command placeholder

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use taskzip::generate;
 use taskzip::package;
 
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use std::path::PathBuf;
 use std::time::Duration;
 
@@ -13,6 +13,17 @@ use std::time::Duration;
 struct Cli {
     #[command(subcommand)]
     cmd: Command,
+}
+
+#[derive(ValueEnum, Clone)]
+enum ExternalFormat {
+    Lio2024,
+}
+
+impl std::fmt::Display for ExternalFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.to_possible_value().unwrap().get_name())
+    }
 }
 
 #[derive(Subcommand)]
@@ -32,6 +43,13 @@ enum Command {
         out: PathBuf,
         #[arg(long, default_value_t = 60)]
         timeout: u64,
+    },
+    #[command(about = "Transform external task format into TaskZip")]
+    Import {
+        #[arg(value_enum)]
+        format: ExternalFormat,
+        src: PathBuf,
+        dest: PathBuf,
     },
     ValidateTests {
         #[arg(default_value = ".")]
@@ -113,6 +131,11 @@ fn main() -> Result<()> {
                 }
                 println!("{}: {}/{}", r.fname, r.score, r.total);
             }
+        }
+        Command::Import { format, src, dest } => {
+            println!("format: {}", format);
+            println!("src: {}", src.display());
+            println!("dest: {}", dest.display());
         }
     }
     Ok(())
